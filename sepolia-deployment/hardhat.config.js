218936021d56cb38824d2ec5f94d084b4c0e2733
Aditya <aditya.keerthi1@uwaterloo.ca>
2023-06-24 22:04:42 -0400

    deployed smart contract

--- sepolia-deployment/hardhat.config.js
+++ sepolia-deployment/hardhat.config.js
@@ -1,4 +1,16 @@
-/** @type import('hardhat/config').HardhatUserConfig */
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+
+const { API_URL, PRIVATE_KEY } = process.env;
+
 module.exports = {
-  solidity: "0.8.18",
-};
+  solidity: "0.7.3",
+  defaultNetwork: "sepolia",
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: API_URL,
+      accounts: [`0x${PRIVATE_KEY}`]
+    }
+  },
+}
